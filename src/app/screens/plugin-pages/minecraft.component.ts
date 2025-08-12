8d4c81b98a12415f6aaa70e92266229c4e83d441
Bug1312 <bug@bug1312.com>
2025-08-11 20:24:05 -0700

    Adding Totoro's Top

--- src/app/screens/plugin-pages/minecraft.component.ts
+++ src/app/screens/plugin-pages/minecraft.component.ts
@@ -9,8 +9,43 @@ import { Plugin } from '../../components/plugin-card/plugin-card.component';
 })
 export class MinecraftComponent {
 
-  plugins: Plugin[] = 
-  [{"title":"Dalek Mod","image":"https://media.forgecdn.net/avatars/thumbnails/601/706/256/256/637981122306949610.png","imageType":"url","description":"Dalek Mod is a Doctor Who oriented Mod. I helped work on DM in 2020-2023 from simple assets to being one of the main developers","moreInfo":"https://swdteam.com/p/dalek-mod","downloadURL":"https://swdteam.com/p/dalek-mod"},{"title":"Ballooning","sharp":true,"image":"https://cdn.modrinth.com/data/J1BCbrXI/1a148a97c0ba44e714afeaada47491ce989460fd.png","imageType":"url","description":"A small mod made for ModFest: Carnival that turns anything lead-able into a balloon","moreInfo":"https://modrinth.com/mod/ballooning","downloadURL":"https://modrinth.com/mod/ballooning"},{"title":"Magician's Hat","sharp":true,"image":"https://cdn.modrinth.com/data/UeEP05XD/82e16ffc93ed97674a27c2c5780cea1c09e4a7d3.png","imageType":"url","description":"A small mod made for ModFest: Carnival that adds a hat used to magically store and expel items as well as become a fashion piece","moreInfo":"https://modrinth.com/mod/magicians_hat","downloadURL":"https://modrinth.com/mod/magicians_hat"}]
-  ;
+  plugins: Plugin[] =
+    [
+      {
+        "title": "Dalek Mod",
+        "image": "https://media.forgecdn.net/avatars/thumbnails/601/706/256/256/637981122306949610.png",
+        "imageType": "url",
+        "description": "Dalek Mod is a Doctor Who oriented Mod. I helped work on DM in 2020-2023 from simple assets to being one of the main developers",
+        "moreInfo": "https://swdteam.com/p/dalek-mod",
+        "downloadURL": "https://swdteam.com/p/dalek-mod"
+      },
+      {
+        "title": "Totoro's Top",
+        "image": "https://cdn.modrinth.com/data/wSGrIhNF/4736c31ea64639dff9e40fc634a1d0b191591728_96.webp",
+        "imageType": "url",
+        "description": "A small mod made for ModFest: ToyBox adds a magical top that can help The Player fly around the skies!",
+        "moreInfo": "https://modrinth.com/mod/totoro_top",
+        "downloadURL": "https://modrinth.com/mod/totoro_top/versions"
+      },
+      {
+        "title": "Ballooning",
+        "sharp": true,
+        "image": "https://cdn.modrinth.com/data/J1BCbrXI/1a148a97c0ba44e714afeaada47491ce989460fd.png",
+        "imageType": "url",
+        "description": "A small mod made for ModFest: Carnival that turns anything lead-able into a balloon",
+        "moreInfo": "https://modrinth.com/mod/ballooning",
+        "downloadURL": "https://modrinth.com/mod/ballooning"
+      },
+      {
+        "title": "Magician's Hat",
+        "sharp": true,
+        "image": "https://cdn.modrinth.com/data/UeEP05XD/82e16ffc93ed97674a27c2c5780cea1c09e4a7d3.png",
+        "imageType": "url",
+        "description": "A small mod made for ModFest: Carnival that adds a hat used to magically store and expel items as well as become a fashion piece",
+        "moreInfo": "https://modrinth.com/mod/magicians_hat",
+        "downloadURL": "https://modrinth.com/mod/magicians_hat"
+      }
+    ]
+    ;
 
 }
